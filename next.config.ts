07c1d77fd33143886761153cdb7d5fe79c16608b
skpaik <sdiptapaik@gmail.com>
2025-10-26 22:47:07 +0100

    - contents updated

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: 'export',
+    basePath: '/machprofil', // If deployed to a subpath
 };
 
 export default nextConfig;
