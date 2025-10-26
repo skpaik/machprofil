6c6e6fc073684100c1acba6b6e295eea20cfd9bf
skpaik <sdiptapaik@gmail.com>
2025-10-26 23:48:48 +0100

    Deploy Next.js static export to GitHub Pages

--- docs/_next/static/c2V9yJscuKRRDNX23XIxy/_buildManifest.js
+++ docs/_next/static/c2V9yJscuKRRDNX23XIxy/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/machprofil//_next/:path+",
+        "destination": "/machprofil/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
