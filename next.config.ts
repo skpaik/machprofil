4bad0ed2505adbad8a714cb9aed7b45fe8cea805
skpaik <sdiptapaik@gmail.com>
2025-10-27 01:08:10 +0100

    - contents updated

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'machprofil';
+const isProd = process.env.NODE_ENV === "production";
+const repoName = process.env.REPO_NAME || "";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    output: 'export',
-    distDir: "docs",
-    basePath: '/machprofil',
-    assetPrefix: '/machprofil/',
+    output: "export",
+    distDir: "out",
+    basePath: isProd && repoName ? `/${repoName}` : "",
+    assetPrefix: isProd && repoName ? `/${repoName}/` : "",
     images: {
         unoptimized: true,
     },
